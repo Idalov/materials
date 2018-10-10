779978770e91f6c4148c2c26e97af3dc54b9cf39
dep24 <dep24cpp@yandex.ru>
2018-10-10 09:58:35 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -2,18 +2,33 @@
 #include "include/G4Element.h"
 #include "include/G4Material.h"
 
+G4Material* sample_mat;
+std::vector<G4Material*>* materials;
+G4NistManager* nist;
+
+void setMaterial(const std::string &m_name){
+    for (auto& item: *materials)
+        if(m_name==item->getM_name())
+            sample_mat = item;
+}
+
+G4Material* addSiO2(){
+    auto mat = new G4Material("SiO2",1*g/cm3,2,kStateSolid);
+    mat->AddElement(nist->FindOrBuildElement("Si"),1);
+    mat->AddElement(nist->FindOrBuildElement("O"),2);
+    return mat;
+}
+
 int main() {
-    G4NistManager* nist = G4NistManager::Instance();
+    nist = G4NistManager::Instance();
 
-    auto mat = new G4Material("H2O",2*g/cm3,2,kStateSolid);
-    mat->AddElement(nist->FindOrBuildElement("H"),2);
-    mat->AddElement(nist->FindOrBuildElement("O"),1);
+    materials = new std::vector<G4Material*>();
 
-    auto mat2 = new G4Material("BGO", 3*g/cm3,3,kStateSolid);
-    mat2->AddElement(nist->FindOrBuildElement("Bi"),50*perCent);
-    mat2->AddElement(nist->FindOrBuildElement("Ge"),40*perCent);
-    mat2->AddElement(nist->FindOrBuildElement("O"),10*perCent);
-
+    materials->push_back(addSiO2());            //возвращаемое значение ваших функций передавать в качестве аргумента этой  функции
+//    materials->push_back(...);
+//    materials->push_back(...);
+//    materials->push_back(...);
+//    materials->push_back(...);
     G4Material::Initialize();
     return 0;
 }
